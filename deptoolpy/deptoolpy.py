f726bdf47040cec06172be99b5f3ecccc220e936
Balázs Indig <indba@digitus.itk.ppke.hu>
2018-10-30 12:34:28 +0100

    Refactor PyJNIus import

--- deptoolpy/deptoolpy.py
+++ deptoolpy/deptoolpy.py
@@ -1,27 +1,50 @@
 #!/usr/bin/python3
 # -*- coding: utf-8, vim: expandtab:ts=4 -*-
 
-
 import os
 
-# Tested on Ubuntu 16.04 64bit with openjdk-8 JDK and JRE installed:
-# sudo apt install openjdk-8-jdk-headless openjdk-8-jre-headless
 
-# Set JAVA_HOME for this session
-try:
-    os.environ['JAVA_HOME']
-except KeyError:
-    os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64/'
+def import_pyjnius(class_path):
+    """
+    PyJNIus can only be imported once per Python interpreter and one must set the classpath before importing...
+    """
+    # Check if autoclass is already imported...
+    if 'autoclass' not in locals() and 'autoclass' not in globals():
 
-os.environ['CLASSPATH'] = os.path.join(os.path.dirname(__file__), 'depTool')
+        # Tested on Ubuntu 16.04 64bit with openjdk-8 JDK and JRE installed:
+        # sudo apt install openjdk-8-jdk-headless openjdk-8-jre-headless
 
+        # Set JAVA_HOME for this session
+        try:
+            os.environ['JAVA_HOME']
+        except KeyError:
+            os.environ['JAVA_HOME'] = '/usr/lib/jvm/java-8-openjdk-amd64/'
 
-# Set path and import jnius for this session
-from jnius import autoclass
+        os.environ['CLASSPATH'] = ':'.join((class_path, os.environ.get('CLASSPATH', ''))).rstrip(':')
+
+        # Set path and import jnius for this session
+        from jnius import autoclass
+    else:
+        import sys
+        from jnius import cast, autoclass  # Dummy autoclass import to silence the IDE
+        class_loader = autoclass('java.lang.ClassLoader')
+        cl = class_loader.getSystemClassLoader()
+        ucl = cast('java.net.URLClassLoader', cl)
+        urls = ucl.getURLs()
+        cp = ':'.join(url.getFile() for url in urls)
+
+        print('Warning: PyJNIus is already imported with the following classpath: {0}'.format(cp), file=sys.stderr)
+
+    # Return autoclass for later use...
+    return autoclass
 
 
 class DepToolPy:
+    class_path = os.path.join(os.path.dirname(__file__), 'depTool')
+
     def __init__(self):
+        # All classes should be imported in __init__()
+        autoclass = import_pyjnius(DepToolPy.class_path)
         self._jstr = autoclass('java.lang.String')
         self._dt = autoclass('hu.nytud.gate.util.DepTool')
         self.target_fields = ['pos', 'fature']
